34fb1d15e670eaecaeba19c1fd249617a6aa9906
root <root@MHicksDesktop.>
2024-11-06 15:01:03 -0600

    Updated Changes to DataSource and Go backend conversion

--- pkg/main.go
+++ pkg/main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"context"
 	"os"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/optimizca/servicenow-grafana/pkg/plugin"
 )
 
 func main() {
 	// Start listening to requests sent from Grafana. This call is blocking so
-	// it won't finish until Grafana shuts down the process or the plugin choose
-	// to exit by itself using os.Exit. Manage automatically manages life cycle
-	// of datasource instances. It accepts datasource instance factory as first
-	// argument. This factory will be automatically called on incoming request
-	// from Grafana to create different instances of SampleDatasource (per datasource
-	// ID). When datasource configuration changed Dispose method will be called and
-	// new datasource instance created using NewSampleDatasource factory.
-	if err := datasource.Manage("servicenow-testplugin-datasource", plugin.NewDatasource, datasource.ManageOpts{}); err != nil {
+	// it won't finish until Grafana shuts down the process or the plugin chooses
+	// to exit by itself using os.Exit. Manage automatically manages the lifecycle
+	// of datasource instances and will use NewDatasource as the factory for creating
+	// new instances.
+	if err := datasource.Manage("servicenow-testplugin-datasource", func(ctx context.Context, settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+		// Attempt to create a new datasource instance
+		ds, err := plugin.NewDatasource(ctx, settings)
+		if err != nil {
+			return nil, err
+		}
+		return ds, nil
+	}, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error(err.Error())
 		os.Exit(1)
 	}
