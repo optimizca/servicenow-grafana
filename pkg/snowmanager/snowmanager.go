eec51136683b8fae28c5fade52f3f24800607e00
Jake Licmo <129340250+woby555@users.noreply.github.com>
2024-11-26 10:23:39 -0600

    SnowManager Misc Methods
    Added miscellaneous methods found at the bottom of SnowManager.ts. Untested.

--- pkg/snowmanager/snowmanager.go
+++ pkg/snowmanager/snowmanager.go
@@ -3,6 +3,7 @@ package snowmanager
 import (
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 
@@ -30,6 +31,36 @@ func NewSNOWManager(options SnowManagerOptions) *SNOWManager {
 		APIClient: apiClient,
 		APIPath:   options.APIPath,
 	}
+}
+
+// RemoveFiltersWithAll removes filters containing '*' from the sysparam query string.
+// For example:
+// Input: "operational_status=1^clusterIN*"
+// Output: "operational_status=1"
+func (sm *SNOWManager) RemoveFiltersWithAll(sysparam string) string {
+	fmt.Println("inside RemoveFiltersWithAll")
+	fmt.Println("starting sysparam:", sysparam)
+
+	inputArray := strings.Split(sysparam, "^")
+
+	var parsedInput []string
+	for _, instance := range inputArray {
+		if !strings.Contains(instance, "*") {
+			parsedInput = append(parsedInput, instance)
+		}
+	}
+
+	// Reconstruct the sysparam string
+	if len(parsedInput) > 1 {
+		sysparam = strings.Join(parsedInput, "^")
+	} else if len(parsedInput) == 1 {
+		sysparam = parsedInput[0]
+	} else {
+		sysparam = ""
+	}
+
+	fmt.Println("return sysparam:", sysparam)
+	return sysparam
 }
 
 func (sm *SNOWManager) ParseBasicSysparm(sysparamQuery string, options map[string]interface{}) string {
@@ -74,6 +105,52 @@ func (sm *SNOWManager) ParseBasicSysparm(sysparamQuery string, options map[strin
 
 	result := strings.Join(sysparm, "^")
 	fmt.Println("END OF PARSE BASIC SYSPARM:", result)
+	return result
+}
+
+// SingleSysParamQuery reformats a sysparam query string into a structured array of objects.
+//
+// Example Input: "column1=value1^column2!=value2"
+// Example Output:
+// [
+//
+//	{column: {value: "column1"}, operator: {value: "="}, value: {value: "value1"}, separator: {value: "^"}},
+//	{column: {value: "column2"}, operator: {value: "!="}, value: {value: "value2"}, separator: {value: "^"}}
+//
+// ]
+func (sm *SNOWManager) SingleSysParamQuery(queryParam string) []map[string]map[string]string {
+	operators := []string{
+		"=", "!=", "ISEMPTY", "ISNOTEMPTY", "ANYTHING", "SAMEAS", "NSAMEAS",
+		"<", ">", "<=", ">=", "BETWEEN", "GT_FIELD", "LT_FIELD", "GT_OR_EQUALS_FIELD",
+		"LT_OR_EQUALS_FIELD", "ON", "NOTON", "RELATIVEGE", "RELATIVELE", "RELATIVEGT",
+		"RELATIVELT", "RELATIVEEE", "MORETHAN", "LESSTHAN", "IN", "NOT IN", "LIKE",
+		"NOT LIKE", "STARTSWITH", "ENDSWITH", "EMPTYSTRING", "DYNAMIC",
+	}
+	sort.Slice(operators, func(i, j int) bool {
+		return len(operators[i]) > len(operators[j])
+	})
+
+	instances := strings.Split(queryParam, "^")
+	var result []map[string]map[string]string
+
+	for _, instance := range instances {
+		for _, operator := range operators {
+			index := strings.Index(instance, operator)
+			if index != -1 {
+				column := strings.TrimSpace(instance[:index])
+				value := strings.TrimSpace(instance[index+len(operator):])
+
+				result = append(result, map[string]map[string]string{
+					"column":    {"value": column},
+					"operator":  {"value": operator},
+					"value":     {"value": value},
+					"separator": {"value": "^"},
+				})
+				break
+			}
+		}
+	}
+
 	return result
 }
 
